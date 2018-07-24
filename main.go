ffdd8c2dfbea014a2a2aeb14f501ace45271163a
zhengfang <215566435@qq.com>
2018-07-24 17:27:22 +0800

    push

--- main.go
+++ main.go
@@ -6,16 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+
 func main() {
-	b,err := ioutil.ReadFile("./test.txt")
-	if err != nil {
-        fmt.Print(err)
-	}
-	str := string(b)
-	fmt.Println(str)
+	
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "application/octet-stream")
+		b,err := ioutil.ReadFile("./version.json")
+		if err != nil {
+       		fmt.Print(err)
+		}
+		str := string(b)
+		fmt.Println(str)
+		// c.Writer.Header().Set("Content-Type", "application/octet-stream")
 		c.JSON(200, gin.H{
 			"message": str,
 		})
